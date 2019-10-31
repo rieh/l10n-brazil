566bdba8a5517b462955c3abbab757cdf642cd8c
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2019-10-31 16:54:17 -0300

    [REM] Wrong query

--- l10n_br_crm/migrations/12.0.1.0.0/post-migration.py
+++ l10n_br_crm/migrations/12.0.1.0.0/post-migration.py
@@ -8,10 +8,5 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     cr = env.cr
     cr.execute(
-        '''UPDATE crm_lead cl SET city_id=(
-        SELECT id FROM res_city WHERE ibge_code=(
-        SELECT ibge_code FROM l10n_br_base_city WHERE id=cl.l10n_br_city_id))
-        ''')
-    cr.execute(
         '''UPDATE crm_lead cl SET street_number=number;
         ''')
