f636a9f18848358ba4bc90d4461288ea86e38490
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-19 10:40:21 -0300

    [IMP] black, isort

--- l10n_br_account_payment_order/__manifest__.py
+++ l10n_br_account_payment_order/__manifest__.py
@@ -3,28 +3,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Brazilian Payment Order',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "KMEE, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-brazil',
-    'category': 'Banking addons',
-    'depends': [
-        'l10n_br_base',
-        'account_payment_order',
-        'account_due_list',
-        'account_cancel',
+    "name": "Brazilian Payment Order",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "category": "Banking addons",
+    "depends": [
+        "l10n_br_base",
+        "account_payment_order",
+        "account_due_list",
+        "account_cancel",
     ],
-    'data': [
-        'views/account_payment_order_menu_views.xml',
-        'views/account_due_list.xml',
-        'views/account_payment_line.xml',
-        'views/account_payment_mode.xml',
+    "data": [
+        "views/account_payment_order_menu_views.xml",
+        "views/account_due_list.xml",
+        "views/account_payment_line.xml",
+        "views/account_payment_mode.xml",
     ],
-    'demo': [
-        'demo/account_payment_order_demo.xml',
-        'demo/account_payment_mode_demo.xml'
+    "demo": [
+        "demo/account_payment_order_demo.xml",
+        "demo/account_payment_mode_demo.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
