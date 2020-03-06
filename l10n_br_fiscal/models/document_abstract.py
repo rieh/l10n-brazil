14a34f3f91827da1de6f9777eadc6cf50283c3e1
Luis Felipe Mileo <mileo@kmee.com.br>
2020-03-06 13:36:00 -0300

    [GAMBI] Remover
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/document_abstract.py
+++ l10n_br_fiscal/models/document_abstract.py
@@ -102,7 +102,7 @@ class DocumentAbstract(models.AbstractModel):
 
     document_type_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.document.type",
-        required=True)
+        )
 
     operation_name = fields.Char(
         string="Operation",
