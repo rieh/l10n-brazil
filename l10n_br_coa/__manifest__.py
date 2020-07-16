613a92715d5515471c8ffc2898e8b6901952ed69
Diego Paradeda <paradeda.diego@gmail.com>
2020-07-16 13:01:24 -0300

    [ADD] MIS Reports for simples nacional

--- l10n_br_coa/__manifest__.py
+++ l10n_br_coa/__manifest__.py
@@ -17,6 +17,7 @@
         'views/l10n_br_account_tax_template.xml',
         "data/account_tax_group_data.xml",
         "data/l10n_br_account_tax_template_data.xml",
+        "data/account_type_data.xml",
     ],
     'demo': [
     ],
