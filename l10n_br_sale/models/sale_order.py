e9eae2f6eadf7b9d55a616a5cbc1d9854938cc71
gabrielcardoso21 <9100543+gabrielcardoso21@users.noreply.github.com>
2020-09-01 10:42:03 -0300

    Update sale_order.py

--- l10n_br_sale/models/sale_order.py
+++ l10n_br_sale/models/sale_order.py
@@ -1,4 +1,4 @@
-,# Copyright (C) 2009  Renato Lima - Akretion
+# Copyright (C) 2009  Renato Lima - Akretion
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
