028db04a29116d90550f41eacbb22e51f04be9a7
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-12-22 15:46:09 -0300

    [ADD] Carry entry data

--- l10n_br_purchase/models/account_invoice.py
+++ l10n_br_purchase/models/account_invoice.py
@@ -9,8 +9,21 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     def _prepare_invoice_line_from_po_line(self, line):
-        values = super()._prepare_invoice_line_from_po_line(line)
-        values.update(line._prepare_br_fiscal_dict(default=False))
+        values = line._prepare_br_fiscal_dict(default=False)
+        values.update(super()._prepare_invoice_line_from_po_line(line))
+        seller = line.product_id._select_seller(
+            partner_id=line.partner_id,
+            quantity=line.product_qty,
+            date=line.order_id.date_order and line.order_id.date_order.date(),
+            uom_id=line.product_uom
+        )
+        values['product_name'] = seller.product_name
+        values['product_code'] = seller.product_code
+        values['qty_received'] = line.product_qty \
+            if line.product_id.purchase_method == 'purchase' \
+            else line.qty_received
+        values['entry_uom_id'] = line.product_uom
+        values['entry_cfop_id'] = line.cfop_id
         return values
 
     @api.onchange('purchase_id')
